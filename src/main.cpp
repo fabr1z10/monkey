779704e327b0e4c8f1e51d4481ab302a7aa80c6d
fabr1z10 <fabventurini@gmail.com>
2023-01-16 17:52:31 +0100

    reading png file in binary! otherwise breaks in Win

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 #include <pybind11/operators.h>
 #include <pybind11/stl.h>
 #include "png.h"
+#include <iostream>
+
 namespace py = pybind11;
+
 
 double add(double x, double y) {
     return x + y;
@@ -11,15 +14,20 @@ double add(double x, double y) {
 
 int read_png(const std::string& file) {
     FILE* infile;
-    infile = fopen(file.c_str(), "r");
+    infile = fopen(file.c_str(), "rb");
     unsigned char sig[8];
 
     /* first do a quick check that the file really is a PNG image; could
      * have used slightly more general png_sig_cmp() function instead */
     fread(sig, 1, 8, infile);
-    if (!png_check_sig(sig, 8))
+	
+	int is_png = png_sig_cmp(sig, 0, 8);
+	std::cout << sig[0] << sig[1] << ", " << is_png<< "\n";
+	
+    if (is_png != 0)
         return 1;
         //exit(1);   /* bad signature */
+
     return 0;
 }
 
