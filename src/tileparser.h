bc3f7b0cb50b88e88cc3f53eeeb7801011c2d4f5
Fabrizio <fabrizio.venturini@optrail.com>
2024-11-07 21:53:55 +0100

    added pal to tileparser

--- src/tileparser.h
+++ src/tileparser.h
@@ -16,7 +16,9 @@ private:
 	std::vector<std::string> tokenize(const std::string& s, const std::string& delimiters);
 	void quad(const std::string& args);
 	void go(const std::string& args);
-	int _x, _y;
+	void pal(const std::string& args);
+
+	int _x, _y, _pal;
 	using FPtr = void (TileLanguageParser::*)(const std::string& args);
 	Trie _tokens;
 	std::unordered_map<std::string, FPtr> _funcs;
